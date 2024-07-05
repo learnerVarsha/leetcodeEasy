410338ea4850c3fad19f668e68d862535fb72dea
learnerVarsha <learner.varsha@gmail.com>
2024-07-05 09:00:09 -0400

    palindrome number in java and python

---
+++ 9.PalindromeNumber/PalindromeNumber.java
@@ -0,0 +1,27 @@
+//Author: Varsha Rajawat
+//Date: 5 July 2024
+//Source: https://leetcode.com/problems/palindrome-number/
+public class PalindromeNumber {
+    public boolean isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
+        int copyX = x;
+        int reverse = 0;
+        while(copyX!=0){
+            int last_num = copyX%10;
+            reverse = (reverse * 10) + last_num;
+            copyX = copyX/10;
+        }
+        if(reverse==x){
+            return true;
+        }
+        return false;
+    }
+    public static void main(String args[]){
+        PalindromeNumber pn = new PalindromeNumber();
+        System.out.println("Is 121 a palindrome?" + pn.isPalindrome(121));
+        System.out.println("Is -121 a palindrome?" + pn.isPalindrome(-121));
+        System.out.println("Is 101 a palindrome?" + pn.isPalindrome(101));
+    }
+}
---
+++ 9.PalindromeNumber/PalindromeNumber.py
@@ -0,0 +1,16 @@
+#Author: Varsha Rajawat
+#Date: 5 July 2024
+#Source: https://leetcode.com/problems/palindrome-number/
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x<0:
+            return False
+        temp = x
+        reverse = 0
+        while temp!=0:
+            last_digit = temp%10
+            reverse = (reverse * 10) + last_digit
+            temp //= 10
+
+        return reverse==x
