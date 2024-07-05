346849e43405e0b8899ab41bca5944e998a89a4a
learnerVarsha <learner.varsha@gmail.com>
2024-07-05 08:13:53 -0400

    python code for two sum

--- 1.TwoSum/TwoSum.java
+++ 1.TwoSum/TwoSum.java
@@ -1,8 +1,15 @@
+//Author: Varsha Rajawat
+//Date: 5 July 2024
+//Source: https://leetcode.com/problems/two-sum/description/?source=submission-ac
+
 // Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 
 // You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
 // You can return the answer in any order.
+
+import java.util.HashMap;
+import java.util.Map;
 
 class Solution {
     public int[] twoSum(int[] nums, int target) {
---
+++ 1.TwoSum/TwoSum.py
@@ -0,0 +1,20 @@
+#Author: Varsha Rajawat
+#Date: 5 July 2024
+#Source: https://leetcode.com/problems/two-sum/description/?source=submission-ac
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        #Create a dictionary to store numbers and their indices
+        nums_dict = {}
+
+        #iterate over the nums list
+        for i, num in enumerate(nums):
+            complement = target - num
+
+            if complement in nums_dict:
+                return [nums_dict[complement], i]
+            
+            nums_dict[num] = i
+        
+        return []
